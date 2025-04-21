8e56c4a517be06b624910ae608cbe6ba7ca44339
yohanangulo <anguloyohan98@gmail.com>
2025-04-20 21:10:05 -0400

    refactor: configure dependencies

--- lib/src/presentation/common/app/main_common.dart
+++ lib/src/presentation/common/app/main_common.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_skill_showcase/src/config/config/config.dart';
+import 'package:flutter_skill_showcase/src/config/service_locator/service_locator.dart';
 import 'package:flutter_skill_showcase/src/presentation/common/app/app.dart';
 
 Future<void> mainCommon(Flavor flavor) async {
   WidgetsFlutterBinding.ensureInitialized();
   await Config.initialize(flavor);
+  await configureDependencies();
   runApp(const App());
 }
