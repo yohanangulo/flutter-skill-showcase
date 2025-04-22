1b36e3227a5323f2a31c5f9a1db74c6b39244f44
yohanangulo <anguloyohan98@gmail.com>
2025-04-22 12:41:49 -0400

    feat: them x

--- lib/src/config/extensions/build_context_extensions.dart
+++ lib/src/config/extensions/build_context_extensions.dart
@@ -4,4 +4,6 @@ extension BuildContextX on BuildContext {
   void showSnackBar(String message) {
     ScaffoldMessenger.of(this).showSnackBar(SnackBar(content: Text(message)));
   }
+
+  ThemeData get theme => Theme.of(this);
 }
